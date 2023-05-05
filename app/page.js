238b6257cd99211c77ce2b227e75c88aae7c0746
danielmdavis <danis1911@gmail.com>
2023-05-05 13:01:38 -0400

    basic reducer & basic form, in tandem but not yet talking

--- app/page.js
+++ app/page.js
@@ -1,94 +1,29 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import React, { useState, useReducer } from 'react'
+import { useForm } from 'react-hook-form'
+import reducer from './state'
 
 export default function Home() {
+
+  const initialState = {count: 0}
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const onSubmit = (data) => console.log(data)
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      Count: {state.count}
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+      <div className='wrap'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label for='key'>Enter Your LoveBot Key</label>
+          <input id='key'type='password' placeholder='key' {...register('LoveBot Key', {required: true})} />
+          <label for='name'>What should I call you?</label>
+          <input id='name' type='text' placeholder='name' {...register('What should I, LoveBot 3000, call you?', {})} />
+          <input type='submit' />
+        </form>
       </div>
     </main>
   )
