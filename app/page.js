9461024b466f024e6e750da8f07cf0a09d4581eb
danielmdavis <danis1911@gmail.com>
2023-05-05 13:47:57 -0400

    meaningful reducer progress in the direction of storing object updates in global.

--- app/page.js
+++ app/page.js
@@ -5,24 +5,30 @@ import reducer from './state'
 
 export default function Home() {
 
-  const initialState = {count: 0}
-  const [state, dispatch] = useReducer(reducer, initialState)
-
   const { register, handleSubmit, formState: { errors } } = useForm()
   const onSubmit = (data) => console.log(data)
 
+
+  const initialCreds = {creds: {key: '', nomen: ''}}
+  let newCreds = {key: 'foo', nomen: 'bar'}
+
+  const [creds, dispatch] = useReducer(reducer, initialCreds);
+
+  const handleCreds = (creds) => {
+    dispatch({ type: 'setCreds', creds: creds });
+  }
+
   return (
     <main>
-      Count: {state.count}
-      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-      <button onClick={() => dispatch({type: 'increment'})}>+</button>
       <div className='wrap'>
         <form onSubmit={handleSubmit(onSubmit)}>
           <label for='key'>Enter Your LoveBot Key</label>
           <input id='key'type='password' placeholder='key' {...register('LoveBot Key', {required: true})} />
           <label for='name'>What should I call you?</label>
           <input id='name' type='text' placeholder='name' {...register('What should I, LoveBot 3000, call you?', {})} />
-          <input type='submit' />
+          {/* <input type='submit' /> */}
+          <button onClick={() => handleCreds(newCreds)}>submit</button>
+
         </form>
       </div>
     </main>
--- app/state.js
+++ app/state.js
@@ -1,11 +1,12 @@
 
-export default function reducer(state, action) {
-    switch (action.type) {
-      case 'increment':
-        return {count: state.count + 1}
-      case 'decrement':
-        return {count: state.count - 1}
-      default:
-        throw new Error()
-    }
+
+export default function reducer (state, action) {
+  switch (action.type) {
+    case 'setCreds':
+      state.creds = action.creds
+      console.log(state)
+      return state
+    default:
+      return state
   }
+}
