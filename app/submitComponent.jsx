7cd3e791dbced4be79242561c938bdd7cf32ccd7
danielmdavis <danis1911@gmail.com>
2023-05-11 13:56:39 -0400

    basic structured email push

--- app/submitComponent.jsx
+++ app/submitComponent.jsx
@@ -5,35 +5,49 @@ export default function Submit(props) {
 
   const sendEmail = e => {
 
-    console.log(props.voteArray)
+    const votes = props.voteArray.map(v => {
+      const vote = Object.entries(v)[0]
+      return `
+      &nbsp;&nbsp;&nbsp;&nbsp;${vote[0]}  ${vote[1]}`
+    })
 
-    const votes = Object.values(props.voteArray)
+    // const styledVotes = `<div style={{ background-color: 'rgb(75,182,203)', color: 'rgb(239,103,69)', font-family: 'Helvetica'}}>
+    //   Want to know who your match really is? Read on.
+
+    //   ${votes}
+    // </div>`
+
+    const styledVotes = 
+    `Want to know who your match really is, deep down? Read on.
+      ${votes}
+
+    If you're still interested, you can reach them at foo@bar.com
+
+    Faithfully yours,
+    LoveBot 3000
+
+
+    `
+    console.log(styledVotes)
+
     const params = {
       email: 'danis1911@gmail.com',
-      message: `
-      ${votes[0]}
-      ${votes[1]}
-      ${votes[2]}
-      ${votes[3]}
-      `
+      message: styledVotes
     }
-
     e.preventDefault()
-
-    emailjs.sendForm('service_6mrou7s', 'template_htabidd', sender, 'LZ8PCPJuf-O1SbJGf', params)
+    emailjs.send('service_6mrou7s', 'template_htabidd', params, 'LZ8PCPJuf-O1SbJGf')
       .then((result) => {
-          // window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+          // window.location.reload() 
       }, (error) => {
           console.log(error.text)
       })
 
   }
 
-
     return (
         <div className='wrap' style={{ fontFamily: 'Space Grotesk' }}>
-        <form id='sender' className={`form fade-in`}>
-          <label className='question-text'>One final question: are prepared to share your answers with not just me, LoveBot 3000, but your crush as well?</label>
+        <form className={`form fade-in`}>
+          <label className='question-text'>One final question: are prepared to share your answers with not just me, LoveBot 3000, but your matches as well?</label>
           <label className='question-text'>I regret to inform you that you will not be shown your matches otherwise. Think it over.</label>
 
             <div className='button-pair'> 
